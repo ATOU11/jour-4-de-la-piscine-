55b5b32f169b9ad8f423893e6ba3348f74acefe6
ATOU11 <prince.atou@epitech.digital>
2024-10-03 14:49:29 +0200

    Create ex_03.js

---
+++ ex_03/ex_03.js
@@ -0,0 +1,4 @@
+const iphoneDetails = document.getElementById("description");
+const newFeature = document.createElement("p");
+newFeature.innerHTML = "Now featuring a headphone jack!";
+iphoneDetails.appendChild(newFeature);
